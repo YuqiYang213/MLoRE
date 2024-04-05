23e70f11749cf8467d92c1f55c5165ddac2e8f38
sakarenko <985558237@qq.com>
2024-04-05 23:13:14 +0800

    edit the dataset path

--- configs/mypath.py
+++ configs/mypath.py
@@ -1,7 +1,7 @@
 import os
 PROJECT_ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).split('/')[0]
 
-db_root = '/home/yyqwsd/dataset'
+db_root = './'
 
 db_names = {'PASCALContext': 'PASCALContext', 'NYUD_MT': 'NYUDv2'}
 db_paths = {}
